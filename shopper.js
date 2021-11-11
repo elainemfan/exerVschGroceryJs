0c85907ed606018115a63114ed3cf7d8b878dd55
Elaine Fan <89660411+elainemfan@users.noreply.github.com>
2021-11-11 12:37:55 -0700

    Add files via upload

---
+++ shopper.js
@@ -0,0 +1,11 @@
+var shopper = {
+    fName: "Elaine", 
+    lName: 'Fan',
+    Age: 15, 
+    ShopsEveryDay: false,
+    groceryCart: ["banana", "pepper", "milk", "lunch meat"],
+    Name: function() {
+        return (this.fName + "" + this.lName )}
+}
+
+    console.log (shopper)
